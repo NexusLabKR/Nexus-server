82e4fd12ebaa7e119cb7781e5f17bd7e367d9a7f
문정훈 <mjh090801abc@gmail.com>
2026-07-20 09:39:51 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/com/Nexus/NexusApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class NexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
