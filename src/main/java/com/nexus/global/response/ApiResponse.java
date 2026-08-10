ebfe299250f6f7b9c49a910330eea5fa4cb15d58
문정훈 <mjh090801abc@gmail.com>
2026-08-10 18:02:34 +0900

    API 응답을 위한 컬럼들 정의

--- src/main/java/com/nexus/global/response/ApiResponse.java
+++ src/main/java/com/nexus/global/response/ApiResponse.java
@@ -1,4 +1,15 @@
 package com.nexus.global.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+// <T>는 응답으로 어떤 데이터가 들어올지 몰라 나중에 결정하겠다는 뜻
+public class ApiResponse<T> {
+
+    private boolean success;
+    private T data;
+    private String message;
+
 }
