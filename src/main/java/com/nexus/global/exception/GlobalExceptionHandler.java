075a4a0bf51c03e7197e91508ac2ec183cc6f39f
문정훈 <mjh090801abc@gmail.com>
2026-08-11 10:26:50 +0900

    예외 발생 시 에러 코드와 함께 에러 메세지를 출력할 수 있도록 메서드 생성

--- src/main/java/com/nexus/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nexus/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.nexus.global.exception;
 
+import com.nexus.global.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
+
+        ErrorCode errorCode = e.getErrorCode();
+
+
+
+    }
 }
